50067997e12866a766283817bed7f7981ae86ed5
Sally Sun <sallyysun101@gmail.com>
2022-08-17 00:49:35 +1000

    changed folders

---
+++ src/components/header/header.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+// import '../../App.css'
+
+
+function Header() {
+  return (
+		<header>
+			<div className="container header__container">
+				<h5>Hello I'm</h5>
+				<h1>Sally Sun</h1>
+				<h5 className='text-light'>UNSW Student</h5>
+				
+			</div>
+		</header>
+  );
+}
+
+export default Header
