a0d24ac55a91ca04f6cb1b9a862ad96d4f17d34e
Sally Sun <sallyysun101@gmail.com>
2022-08-16 20:33:55 +1000

    created react app

---
+++ about_me/src/components/NavBar.js
@@ -0,0 +1,17 @@
+import React, {useState} from 'react'
+
+function NavBar() {
+  return (
+		<>
+			<nav className="nav">
+				<div className="navbar-container">
+					<Link>
+
+					</Link>
+				</div>
+			</nav>
+		</>
+  )
+}
+
+export default NavBar
