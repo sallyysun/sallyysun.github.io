78d6d4bf315f07ca6418f17fe5e876fb57beecb2
Sally Sun <sallyysun101@gmail.com>
2022-08-17 01:05:11 +1000

    header

--- src/components/header/CTA.jsx
+++ src/components/header/CTA.jsx
@@ -4,8 +4,8 @@ import CV from '../../assets/resume2206.pdf'
 function CTA() {
 	return (
 		<div className='cta'>
-			<a href="">Download CV</a>
-			<a href="#contact">Socials</a>
+			<a href={CV} download className='btn'>My CV</a>
+			<a href="#contact" className='btn btn-primary'>Socials</a>
 		</div>
 	)
 }
--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,15 +1,21 @@
 import React from 'react'
-// import '../../App.css'
-
+import './header.css'
+import CTA from './CTA'
+import PIC from '../../assets/pic.png'
 
 function Header() {
   return (
 		<header>
 			<div className="container header__container">
-				<h5>Hello I'm</h5>
+				<h3>Hello I'm</h3>
 				<h1>Sally Sun</h1>
-				<h5 className='text-light'>UNSW Student</h5>
-				
+				<h4 className='text-light'>UNSW Student</h4>
+				<CTA />
+
+				<div className="me">
+					<img src={PIC} alt="" />
+				</div>
+
 			</div>
 		</header>
   );
