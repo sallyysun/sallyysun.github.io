37ad36e64cf75b4c89293ff7ce773bfbd2b69be7
Sally Sun <sallyysun101@gmail.com>
2022-08-24 16:22:28 +1000

    about wrote smth

--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -6,6 +6,27 @@ function About() {
 		<section id='about'>
 			<h3>About Me ...</h3>
 			<h2>some fun facts about me</h2>
+			
+			<div className="container about__container">
+				<div>
+					<h3>1</h3>
+					<h4>I have a scar on my forehead from when my mum dropped me as a baby.</h4>
+				</div>
+
+				<div>
+					<h3>2</h3>
+					<h4>My favourite colour is purple.</h4>
+				</div>
+
+				<div>
+					<h3>3</h3>
+					<h4>I've changed degrees 3 times since 2021...</h4>
+					<h5>Commerce / Computer Science</h5>
+					<h5>Commerce Information Systems (Coop)</h5>
+					<h5>Commerce / Computer Science</h5>
+					<h5>Science / Computer Science</h5>
+				</div>
+			</div>
 		</section>
 	)
 }
--- src/components/experiences/experiences.jsx
+++ src/components/experiences/experiences.jsx
@@ -14,16 +14,22 @@ function Experiences() {
 					<h5>July 2021 - Current</h5>
 				</div>
 
-				<div className="experience__light">
+				<div>
 					<h4>UNSW Business Society</h4>
 					<h3>Education Director</h3>
 					<h5>November 2021 - Current</h5>
 				</div>
 
-				<div className="experience__dark">
+				<div>
 					<h4>UNSW CSESoc</h4>
 					<h3>Circles Frontend</h3>
 					<h5>February 2021 - November 2021</h5>
+				</div>
+
+				<div>
+					<h4>Self-Employed</h4>
+					<h3>3U & 4U Mathematics Tutor</h3>
+					<h5>February 2019 - Current</h5>
 				</div>
 			</div>
 		</section>
