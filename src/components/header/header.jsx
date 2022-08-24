f2addd979bd2ecfedc936ea8b232f70600f36b29
Sally Sun <sallyysun101@gmail.com>
2022-08-24 16:51:41 +1000

    added pic

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import './header.css'
 import CTA from './CTA'
 import HeaderSocials from './HeaderSocials'
+import DP from '../../assets/dp.JPG'
 
 // import PIC from '../../assets/pic.png'
 
@@ -14,9 +15,9 @@ function Header() {
 				<h4 className='text-light'>2nd Year Computer Science / Science Student</h4>
 				<CTA />
 				<HeaderSocials />
-				{/* <div className="me">
-					<img src={PIC} alt="" />
-				</div> */}
+				<div className="me">
+					<img src={DP} alt="dp" />
+				</div>
 
 			</div>
 		</header>
