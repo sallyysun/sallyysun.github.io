11d3ad4c1e07b5099b354cfd552beb1d5e6ef103
Sally Sun <sallyysun101@gmail.com>
2022-08-25 00:36:39 +1000

    fixed pics

--- src/components/experiences/experiences.jsx
+++ src/components/experiences/experiences.jsx
@@ -1,6 +1,8 @@
 import React from 'react'
 import './experiences.css'
 import BSOC1 from '../../assets/bsoc1bigger.JPG'
+import UBS1 from '../../assets/ubs1.JPG'
+import CIRCLES2 from '../../assets/circles2.PNG'
 
 function Experiences() {
 	return (
@@ -10,28 +12,39 @@ function Experiences() {
 
 			<div className="container experience__container">
 				<div className='experience__content'>
-					<h4>UBS</h4>
-					<h3>Tech Cadet</h3>
-					<h5>July 2021 - Current</h5>
-					
+					<div className="content">
+						<h4>UBS</h4>
+						<h3>Tech Cadet</h3>
+						<h5>July 2021 - Current</h5>
+					</div>
+					<div className="pictures fade">
+						<img src={UBS1} alt="ubs1" />
+					</div>
+				</div>
+
+				<div className='experience__content'>
+					<div className="content">
+						<h4>UNSW Business Society</h4>
+						<h3>Education Director</h3>
+						<h5>November 2021 - Current</h5>
+					</div>
 					<div className="pictures fade">
 						<img src={BSOC1} alt="bsoc1bigger" />
 					</div>
 				</div>
 
-				<div>
-					<h4>UNSW Business Society</h4>
-					<h3>Education Director</h3>
-					<h5>November 2021 - Current</h5>
+				<div className='experience__content'>
+					<div className="content">
+						<h4>UNSW CSESoc</h4>
+						<h3>Circles Frontend</h3>
+						<h5>February 2021 - November 2021</h5>
+					</div>
+					<div className="pictures fade">
+						<img src={CIRCLES2} alt="circles2" />
+					</div>
 				</div>
 
-				<div>
-					<h4>UNSW CSESoc</h4>
-					<h3>Circles Frontend</h3>
-					<h5>February 2021 - November 2021</h5>
-				</div>
-
-				<div>
+				<div className='experience__content'>
 					<h4>Self-Employed</h4>
 					<h3>3U & 4U Mathematics Tutor</h3>
 					<h5>February 2019 - Current</h5>
