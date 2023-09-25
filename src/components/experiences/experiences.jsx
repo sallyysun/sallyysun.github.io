aebe33cf3562ae554fd21726fbd686d2ff3d894d
Sally Sun <sallyysun101@gmail.com>
2023-09-25 19:09:44 +1000

    style

--- src/components/experiences/experiences.jsx
+++ src/components/experiences/experiences.jsx
@@ -15,15 +15,15 @@ function Experiences() {
         <div className="experience__content">
           <div className="content">
             <h2>Circles Subcom</h2>
-            <h5>2021</h5>
+            <h4>2021</h4>
           </div>
-          <h4 className="text-light">
+          <h5 className="text-light">
             I was Circles subcommittee under James and Jenn, and we ideated,
             designed and built the beginning of the Circles you see now. Circles
             was the reason I decided to switch degrees to Computer Science at
             the end of the year. I learnt so much about front-end development
             this year and really treasure this opportunity.
-          </h4>
+          </h5>
           <div className="pictures fade">
             <img src={CIRCLES1} alt="circles1" />
             <img src={CIRCLES2} alt="circles2" />
@@ -33,13 +33,13 @@ function Experiences() {
         <div className="experience__content">
           <div className="content">
             <h2>Trainee Program & Notangles Subcom</h2>
-            <h5>2022 T3 & 2023</h5>
+            <h4>2022 T3 & 2023</h4>
           </div>
-          <h4 className="text-light">
+          <h5 className="text-light">
             I participated in the Trainee Program in 2022 T3, and together with
             Notangles this year I've met so many amazing people who have all
             been really lovely and so incredibly hardworking.
-          </h4>
+          </h5>
           <h5>
             Shoutout to MJ, Raiyan, Rachel, Manhua, Wanning, Sijin, Jasmine,
             Shaam, Eklavya, Michael
@@ -52,14 +52,14 @@ function Experiences() {
         <div className="experience__content">
           <div className="content">
             <h2>Education Director</h2>
-            <h5>2022 (in bsoc)</h5>
-            <h4 className="text-light">
+            <h4>2022 (in bsoc)</h4>
+            <h5 className="text-light">
               Throughout this experience I organised a Peer Mentoring Program
               with 600+ attendees, as well as put some of my own ideas into
               action, such as Linkedin Workshop, a Business Majors Fair and
               more. This is experience has what honed my ability to lead a group
               of people to achieve a common goal.
-            </h4>
+            </h5>
           </div>
           <div className="pictures fade">
             <img src={BSOC1} alt="bsoc1" />
